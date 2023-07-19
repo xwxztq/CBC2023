155ef530a60d16c19a0b87c424ace49d27abd408
xwxztq <834026555@qq.com>
2023-07-19 14:18:01 +0800

    🐛fix docker run command without passing gpus

--- Q1/run.sh
+++ Q1/run.sh
@@ -3,4 +3,4 @@ docker login --username $1 --password $2 $3
 docker pull $3
 
 mkdir -p datas/out
-docker run --rm -it -v `pwd`/datas:/mounted/ $3 bash run.sh  /mounted/in.lst /mounted/out
+docker run --rm -it --ipc=host --gpus all -v `pwd`/datas:/mounted/ $3 bash run.sh  /mounted/in.lst /mounted/out
